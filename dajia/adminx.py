a1701e61a034cd744e84916a6f7895f69aa3a30f
liurongjie <1397608894@qq.com>
2019-04-05 15:32:08 +0800

    15:32

--- dajia/adminx.py
+++ dajia/adminx.py
@@ -1,6 +1,22 @@
 import xadmin
-
+from xadmin import views
 from .models import Team,User,Merchant,Production,Period,Steam,Comment,Order,Cutting
+# 创建xadmin的最基本管理器配置，并与view绑定
+class BaseSetting(object):
+    # 开启主题功能
+    enable_themes = True
+    use_bootswatch = True
+
+# 全局修改，固定写法
+class GlobalSettings(object):
+    site_title='Boat后台管理系统'
+    site_footer="珞珈云顶工坊"
+    menu_style = 'accordion'
+# 将基本配置管理与view绑定
+xadmin.site.register(views.BaseAdminView,BaseSetting)
+
+# 将title和footer信息进行注册
+xadmin.site.register(views.CommAdminView,GlobalSettings)
 
 class Teamxadmin(object):
     list_display={'teamid','teamname','logo','time'}
--- dajia/apps.py
+++ dajia/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class DajiaConfig(AppConfig):
     name = 'dajia'
+    verbose_name='表格'
